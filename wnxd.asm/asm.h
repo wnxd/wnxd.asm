9a03845066e37f44640522fa1d025dff0eabbff0
为你芯冻 <514543271@qq.com>
2015-09-14 09:10:50 +0800

    1.4

--- wnxd.asm/asm.h
+++ wnxd.asm/asm.h
@@ -66,56 +66,134 @@ namespace wnxd
 				VirtualProtectEx((void*)Process::GetCurrentProcess()->Handle, address, size, PAGE_EXECUTE_READWRITE, &old);
 			}
 		public:
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun(IntPtr ptr, array<Object^>^ args)
 			{
 				PtrRun<int>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun(void* ptr, array<Object^>^ args)
 			{
 				PtrRun<int>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun(int ptr, array<Object^>^ args)
 			{
 				PtrRun<int>(ptr, args);
 			}
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun(IntPtr ptr, array<Object^>^ args)
 			{
 				return PtrRun<TResult>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun(void* ptr, array<Object^>^ args)
 			{
 				return PtrRun<TResult>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun(int ptr, array<Object^>^ args)
 			{
 				const array<Byte>^ code = { 85, 137, 229, 81, 82, 83, 139, 69, 12, 139, 72, 4, 133, 201, 116, 85, 131, 192, 12, 139, 85, 8, 131, 194, 8, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 137, 223, 131, 249, 1, 116, 56, 131, 192, 4, 131, 194, 1, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 137, 222, 131, 249, 2, 116, 30, 131, 233, 2, 107, 217, 4, 1, 216, 1, 202, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 83, 131, 232, 4, 131, 234, 1, 226, 236, 137, 242, 137, 249, 139, 69, 248, 255, 208, 91, 201, 194, 4, 0 };
 				return _PtrRun<TResult>(ptr, code, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun_Impl(IntPtr ptr, array<Object^>^ args)
 			{
 				PtrRun_Impl<int>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun_Impl(void* ptr, array<Object^>^ args)
 			{
 				PtrRun_Impl<int>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
 			static void PtrRun_Impl(int ptr, array<Object^>^ args)
 			{
 				PtrRun_Impl<int>(ptr, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun_Impl(IntPtr ptr, array<Object^>^ args)
 			{
 				return PtrRun_Impl<TResult>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun_Impl(void* ptr, array<Object^>^ args)
 			{
 				return PtrRun_Impl<TResult>((int)ptr, args);
 			}
+			/// <summary>
+			/// 执行一个非IL方法地址
+			/// </summary>
+			/// <typeparam name="TResult">返回值类型</typeparam>
+			/// <param name="ptr">方法地址</param>
+			/// <param name="args">参数列表</param>
+			/// <returns>执行结果</returns>
 			generic <typename TResult> static TResult PtrRun_Impl(int ptr, array<Object^>^ args)
 			{
 				const array<Byte>^ code = { 85, 137, 229, 81, 82, 83, 139, 69, 12, 139, 72, 4, 133, 201, 116, 84, 131, 192, 12, 139, 85, 8, 131, 194, 8, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 137, 223, 131, 249, 1, 116, 55, 131, 192, 4, 131, 194, 1, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 137, 222, 131, 249, 2, 116, 29, 131, 192, 4, 131, 194, 1, 131, 233, 2, 138, 26, 132, 219, 139, 24, 116, 3, 139, 91, 4, 83, 131, 192, 4, 131, 194, 1, 226, 236, 137, 242, 137, 249, 139, 69, 248, 255, 208, 91, 201, 194, 4, 0 };
 				return _PtrRun<TResult>(ptr, code, args);
 			}
+			/// <summary>
+			/// 从数据地址中读取指定长度的字节码
+			/// </summary>
+			/// <param name="ptr">数据地址</param>
+			/// <param name="len">读取长度</param>
+			/// <returns>字节码</returns>
 			static array<Byte>^ PtrToBin(IntPtr ptr, int len)
 			{
 				if (ptr == IntPtr::Zero) return nullptr;
@@ -123,22 +201,52 @@ namespace wnxd
 				for (int i = 0; i < len; i++) b[i] = Marshal::ReadByte(ptr, i);
 				return b;
 			}
+			/// <summary>
+			/// 从数据地址中读取指定长度的字节码
+			/// </summary>
+			/// <param name="ptr">数据地址</param>
+			/// <param name="len">读取长度</param>
+			/// <returns>字节码</returns>
 			static array<Byte>^ PtrToBin(int ptr, int len)
 			{
 				return PtrToBin((IntPtr)ptr, len);
 			}
+			/// <summary>
+			/// 从数据地址中读取指定长度的字节码
+			/// </summary>
+			/// <param name="ptr">数据地址</param>
+			/// <param name="len">读取长度</param>
+			/// <returns>字节码</returns>
 			static array<Byte>^ PtrToBin(void* ptr, int len)
 			{
 				return PtrToBin((IntPtr)ptr, len);
 			}
+			/// <summary>
+			/// 数据地址转化为对象
+			/// </summary>
+			/// <typeparam name="T">对象类型</typeparam>
+			/// <param name="ptr">数据地址</param>
+			/// <returns>对象</returns>
 			generic <typename T> static T PtrToObj(IntPtr ptr)
 			{
 				return PtrToObj<T>((int)ptr);
 			}
+			/// <summary>
+			/// 数据地址转化为对象
+			/// </summary>
+			/// <typeparam name="T">对象类型</typeparam>
+			/// <param name="ptr">数据地址</param>
+			/// <returns>对象</returns>
 			generic <typename T> static T PtrToObj(void* ptr)
 			{
 				return PtrToObj<T>((int)ptr);
 			}
+			/// <summary>
+			/// 数据地址转化为对象
+			/// </summary>
+			/// <typeparam name="T">对象类型</typeparam>
+			/// <param name="ptr">数据地址</param>
+			/// <returns>对象</returns>
 			generic <typename T> static T PtrToObj(int ptr)
 			{
 				array<Byte>^ _code = { 137, 208, 195 };
@@ -161,18 +269,38 @@ namespace wnxd
 				Marshal::FreeHGlobal(_ptr);
 				return ret;
 			}
+			/// <summary>
+			/// 获取方法地址
+			/// </summary>
+			/// <param name="method"></param>
+			/// <returns>方法地址</returns>
 			static IntPtr GetPtr(Delegate^ method)
 			{
 				return GetPtr(method, false);
 			}
+			/// <summary>
+			/// 获取方法地址
+			/// </summary>
+			/// <param name="method"></param>
+			/// <returns>方法地址</returns>
 			static IntPtr GetPtr(Delegate^ method, bool forcibly)
 			{
 				return GetPtr(method->Method, forcibly);
 			}
+			/// <summary>
+			/// 获取方法地址
+			/// </summary>
+			/// <param name="method"></param>
+			/// <returns>方法地址</returns>
 			static IntPtr GetPtr(MethodBase^ method)
 			{
 				return GetPtr(method, false);
 			}
+			/// <summary>
+			/// 获取方法地址
+			/// </summary>
+			/// <param name="method"></param>
+			/// <returns>方法地址</returns>
 			static IntPtr GetPtr(MethodBase^ method, bool forcibly)
 			{
 				IntPtr ptr;
--- wnxd.asm/hook.h
+++ wnxd.asm/hook.h
@@ -41,42 +41,124 @@ namespace wnxd
 				this->_destination = destination;
 			}
 		public:
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
+			/// <param name="destination">转跳的目标方法</param>
 			static void Register(Delegate^ source, Delegate^ destination)
 			{
 				Register(source, destination, false);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
 			static void Register(Delegate^ source, Delegate^ destination, bool forcibly)
 			{
 				Register(source->Method, destination->Method, forcibly);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(IntPtr source, int sourcetoken, Delegate^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Register((int)source, sourcetoken, destination->Method, forcibly, methodhandle, il, external);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(void* source, int sourcetoken, Delegate^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Register((int)source, sourcetoken, destination->Method, forcibly, methodhandle, il, external);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(int source, int sourcetoken, Delegate^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Register(source, sourcetoken, destination->Method, forcibly, methodhandle, il, external);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
+			/// <param name="destination">转跳的目标方法</param>
 			static void Register(MethodBase^ source, MethodBase^ destination)
 			{
 				Register(source, destination, false);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
 			static void Register(MethodBase^ source, MethodBase^ destination, bool forcibly)
 			{
 				Register(Asm::GetPtr(source, forcibly), source->MetadataToken, destination, forcibly, (int)source->MethodHandle.Value, source->GetMethodBody() != nullptr, Asm::IsExternal(source));
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(IntPtr source, int sourcetoken, MethodBase^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Register((int)source, sourcetoken, destination, forcibly, methodhandle, il, external);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(void* source, int sourcetoken, MethodBase^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Register((int)source, sourcetoken, destination, forcibly, methodhandle, il, external);
 			}
+			/// <summary>
+			/// hook一个方法,使其被调用时会转跳到目标方法
+			/// </summary>
+			/// <param name="source">被hook的方法地址</param>
+			/// <param name="sourcetoken">被hook的方法标识</param>
+			/// <param name="destination">转跳的目标方法</param>
+			/// <param name="forcibly">是否强制hook,当普通hook失效时</param>
+			/// <param name="methodhandle">被hook的方法句柄</param>
+			/// <param name="il"></param>
+			/// <param name="external"></param>
 			static void Register(int source, int sourcetoken, MethodBase^ destination, bool forcibly, int methodhandle, bool il, bool external)
 			{
 				Info^ info = gcnew Info();
@@ -116,14 +198,26 @@ namespace wnxd
 				int offset = ldesptr - source - 5;
 				Marshal::WriteInt32(ptr, 1, offset);
 			}
+			/// <summary>
+			/// 取消此方法的hook
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
 			static void Unregister(Delegate^ source)
 			{
 				Unregister(source->Method->MetadataToken);
 			}
+			/// <summary>
+			/// 取消此方法的hook
+			/// </summary>
+			/// <param name="source">被hook的方法</param>
 			static void Unregister(MethodBase^ source)
 			{
 				Unregister(source->MetadataToken);
 			}
+			/// <summary>
+			/// 取消此方法的hook
+			/// </summary>
+			/// <param name="sourcetoken">被hook的方法标识</param>
 			static void Unregister(int sourcetoken)
 			{
 				if (_hooklist->ContainsKey(sourcetoken))
@@ -145,18 +239,39 @@ namespace wnxd
 				}
 
 			}
+			/// <summary>
+			/// 从该对象中寻找指定方法
+			/// </summary>
+			/// <param name="target">包含此方法的对象</param>
+			/// <param name="methodname">方法名</param>
+			/// <returns></returns>
 			static MethodBase^ GetMethod(Object^ target, String^ methodname)
 			{
 				return GetMethod(target->GetType(), methodname);
 			}
+			/// <summary>
+			/// 从该类型中寻找指定方法
+			/// </summary>
+			/// <param name="type">包含此方法的类型</param>
+			/// <param name="methodname">方法名</param>
+			/// <returns></returns>
 			static MethodBase^ GetMethod(Type^ type, String^ methodname)
 			{
 				return type->GetMethod(methodname, BindingFlags::Instance | BindingFlags::Static | BindingFlags::Public | BindingFlags::NonPublic);
 			}
+			/// <summary>
+			/// 强制转换对象类型
+			/// </summary>
+			/// <typeparam name="T">目标类型</typeparam>
+			/// <param name="target">需要转换类型的对象</param>
+			/// <returns>转换类型后的对象</returns>
 			generic <typename T> static T Change(Object^ target)
 			{
 				return (T)target;
 			}
+			/// <summary>
+			/// 切换hook对象的开关
+			/// </summary>
 			void Switch()
 			{
 				if (this->_ishook) Unregister(this->_sourcetoken);
@@ -168,11 +283,21 @@ namespace wnxd
 				}
 				this->_ishook = !this->_ishook;
 			}
+			/// <summary>
+			/// 改变hook对象的开关状态
+			/// </summary>
+			/// <param name="ishook">是否开启hook</param>
 			void Switch(bool ishook)
 			{
 				this->_ishook = !ishook;
 				this->Switch();
 			}
+			/// <summary>
+			/// 调用被hook对象的原方法
+			/// </summary>
+			/// <typeparam name="TResult">返回类型</typeparam>
+			/// <param name="args">参数列表</param>
+			/// <returns>返回值</returns>
 			generic <typename TResult> TResult Run(array<Object^>^ args)
 			{
 				this->Switch();
@@ -180,6 +305,10 @@ namespace wnxd
 				this->Switch();
 				return ret;
 			}
+			/// <summary>
+			/// 调用被hook对象的原方法
+			/// </summary>
+			/// <param name="args">参数列表</param>
 			void Run(array<Object^>^ args)
 			{
 				this->Switch();
@@ -187,6 +316,9 @@ namespace wnxd
 				else Asm::PtrRun_Impl(this->_info->Source, args);
 				this->Switch();
 			}
+			/// <summary>
+			/// 如果当前方法为转跳目标方法,则获取以此为目标的第一个hook对象,否则为null
+			/// </summary>
 			static property Hook^ Source
 			{
 				Hook^ get(){
@@ -202,14 +334,29 @@ namespace wnxd
 					return nullptr;
 				}
 			}
+			/// <summary>
+			/// 获取指定方法的hook对象,如果传入方法未被当前方法hook则返回null
+			/// </summary>
+			/// <param name="method">被hook的方法</param>
+			/// <returns></returns>
 			static Hook^ GetSource(Delegate^ method)
 			{
 				return _GetSource(method->Method->MetadataToken, (gcnew StackFrame(1))->GetMethod());
 			}
+			/// <summary>
+			/// 获取指定方法的hook对象,如果传入方法未被当前方法hook则返回null
+			/// </summary>
+			/// <param name="method">被hook的方法</param>
+			/// <returns></returns>
 			static Hook^ GetSource(MethodBase^ method)
 			{
 				return _GetSource(method->MetadataToken, (gcnew StackFrame(1))->GetMethod());
 			}
+			/// <summary>
+			/// 获取指定方法的hook对象,如果传入方法未被当前方法hook则返回null
+			/// </summary>
+			/// <param name="methodtoken">被hook的方法标识</param>
+			/// <returns></returns>
 			static Hook^ GetSource(int methodtoken)
 			{
 				return _GetSource(methodtoken, (gcnew StackFrame(1))->GetMethod());
